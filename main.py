bd61663e1192abfb723e954b28de94b9de5f9d14
Lubin <lubin.benoit@gmail.com>
2025-10-27 20:25:40 +0100

    Résolution des imports, ajout d'une fonction test dans get_data pour illustrer

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.utils.get_data import *
+
+def main():
+    print("=== Lancement du test ===")
+    test()
+    # les fonctions de get_data.py et autres fichiers dans les sous-dossiers ne peuvent être exécutées que depuis main.py
+
+if __name__ == "__main__":
+    main()
