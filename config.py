27100c0c408ddeef329eba98a121cf62f38604d6
Lubin <lubin.benoit@gmail.com>
2025-10-27 19:40:05 +0100

    ajout de plusieurs fichiers + tentative d'import des variables globales dans get_data.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 
-https://mesure-radioactivite.fr/#/expert
-https://www.data.gouv.fr/datasets/donnees-changement-climatique-sim-quotidienne/#/resources/92065ec0-ea6f-4f5e-8827-4344179c0a7f
+ASNR_RADIATION_BASE_URL = "https://mesure-radioactivite.fr/#/expert"
+METEOFRANCE_WEATHER_DOWNLOAD_URL = "https://www.data.gouv.fr/api/1/datasets/r/92065ec0-ea6f-4f5e-8827-4344179c0a7f"
+VILLEDEREVE_MUNICIPALITY_DOWNLOAD_URL = "https://www.data.gouv.fr/api/1/datasets/r/f5df602b-3800-44d7-b2df-fa40a0350325"
