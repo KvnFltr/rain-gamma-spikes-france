68d1fc663a95d9bbea7bcb563992d1f967d08a4e
Lubin <lubin.benoit@gmail.com>
2025-10-28 18:29:20 +0100

    Remplissage partiel de la fonction de téléchargement du jeu de données sur les radiations

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-ASNR_RADIATION_BASE_URL = "https://mesure-radioactivite.fr/#/expert"
+ASNR_RADIATION_URL = "https://mesure-radioactivite.fr/#/expert"
 METEOFRANCE_WEATHER_DOWNLOAD_URL = "https://www.data.gouv.fr/api/1/datasets/r/92065ec0-ea6f-4f5e-8827-4344179c0a7f"
 VILLEDEREVE_MUNICIPALITY_DOWNLOAD_URL = "https://www.data.gouv.fr/api/1/datasets/r/f5df602b-3800-44d7-b2df-fa40a0350325"
 TIMEOUT = 5000 # Timeout en millisecondes pour les actions Playwright
--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 from playwright.sync_api import sync_playwright, TimeoutError
 
-from config import (ASNR_RADIATION_BASE_URL, 
+from config import (ASNR_RADIATION_URL, 
                     METEOFRANCE_WEATHER_DOWNLOAD_URL, 
                     VILLEDEREVE_MUNICIPALITY_DOWNLOAD_URL,
                     TIMEOUT
@@ -49,49 +49,20 @@ def _safe_playwright_action(description, action):
         print(f"⚠️ Erreur lors de {description} : {e}")
 
 
-def _click_on_element(page, selector, button_selector, description):
+def _click_on_element(page, container_selector, button_selector, description):
     """
     Clique sur un élément identifié par un sélecteur.
 
     Args:
         page: L'objet Playwright Page.
-        selector: Le sélecteur CSS de l'élément à attendre.
+        container_selector: Le sélecteur CSS de l'élément à attendre.
         button_selector: Le sélecteur CSS du bouton à cliquer.
         description: Description textuelle de l'action pour les logs.
     """
-
     def action():
-        page.wait_for_selector(selector, timeout=TIMEOUT)
+        page.wait_for_selector(container_selector, timeout=TIMEOUT)
         button = page.locator(button_selector)
         button.click()
-    _safe_playwright_action(description, action)
-
-
-
-def _fill_field(page, container_selector, field_selector, value, description):
-    """
-    Remplit un champ de formulaire avec une valeur donnée.
-
-    Args:
-        page: L'objet Playwright Page.
-        container_selector: Sélecteur CSS du conteneur du champ.
-        field_selector: Sélecteur CSS du champ à remplir.
-        value: Valeur à entrer dans le champ.
-        description: Description textuelle de l'action pour les logs.
-    """
-    def action():
-        # Localiser le conteneur
-        container = page.locator(container_selector)
-        container.wait_for(state="visible", timeout=10000)
-
-        # Localiser et interagir avec le champ
-        field = container.locator(field_selector)        
-        field.click()
-        page.wait_for_timeout(TIMEOUT)  # Attendre un court délai si nécessaire
-        field.fill("")  # Effacer le contenu existant
-        field.fill(value)  # Remplir avec la nouvelle valeur
-        field.press("Escape")  # Valider (si nécessaire)
-
     _safe_playwright_action(description, action)
 
 
@@ -123,12 +94,31 @@ def _select_dropdown_option(page, container_selector, button_selector, option_li
     _safe_playwright_action(description, action)
 
 
+def _fill_field(page, container_selector, field_selector, value, description):
+    """
+    Remplit un champ de formulaire avec une valeur donnée.
 
+    Args:
+        page: L'objet Playwright Page.
+        container_selector: Sélecteur CSS du conteneur du champ.
+        field_selector: Sélecteur CSS du champ à remplir.
+        value: Valeur à entrer dans le champ.
+        description: Description textuelle de l'action pour les logs.
+    """
+    def action():
+        # Localiser le conteneur
+        container = page.locator(container_selector)
+        container.wait_for(state="visible", timeout=10000)
 
+        # Localiser et interagir avec le champ
+        field = container.locator(field_selector)        
+        field.click()
+        page.wait_for_timeout(TIMEOUT)  # Attendre un court délai si nécessaire
+        field.fill("")  # Effacer le contenu existant
+        field.fill(value)  # Remplir avec la nouvelle valeur
+        field.press("Escape")  # Valider (si nécessaire)
 
-
-
-
+    _safe_playwright_action(description, action)
 
 
 def get_data_ASNR():
@@ -136,7 +126,7 @@ def get_data_ASNR():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto("https://mesure-radioactivite.fr/#/expert", wait_until="domcontentloaded", timeout=60000)
+        page.goto(ASNR_RADIATION_URL, wait_until="domcontentloaded", timeout=60000)
 
         # 1. Fermer la modale informative si elle est présente
         _click_on_element(page, 
@@ -145,10 +135,28 @@ def get_data_ASNR():
                           "Fermeture de la modale informative")
         
         # 2. Sélectionner le menu déroulant "Milieu de collecte" et choisir "Sol"
+        _select_dropdown_option(page,
+                                "div.row.container-select:has(div.label-select:has-text('Milieu de collecte'))",
+                                ".selectric .button",
+                                ".selectricItems",
+                                "Sol",
+                                "Sélection du milieu de collecte 'Sol'"
+                                )
 
         # 3. Choisir la date de début de la sélection
-
+        _fill_field(page,
+                    "div.row.container-select:has(div.label-select:has-text('Date de début'))",
+                    "input.form-control",
+                    "01-janvier-2024",
+                    "Saisie de la date de début de la sélection"
+                    )
         # 4. Choisir la date de fin de la sélection
+        _fill_field(page,
+                    "div.row.container-select:has(div.label-select:has-text('Date de fin'))",
+                    "input.form-control",
+                    "01-janvier-2025",
+                    "Saisie de la date de fin de la sélection"
+                    )
 
         # 5. Refuser les cookies si la bannière est présente
         _click_on_element(page, 
@@ -157,7 +165,21 @@ def get_data_ASNR():
                           "Refus des cookies")
 
         # 6. Cliquer sur "Afficher les résultats"
-
+        _click_on_element(page,
+                          "div.row.container-select:has(button[ng-click='showResult()'])",
+                          "button.btn.little-margin.middle-size.color-purple[ng-click='showResult()']",
+                          "Clic sur 'Afficher les résultats'"
+                          )
+        
         # 7. Cliquer sur l'onglet "Téléchargement"
-
+        _click_on_element(page,
+                          "ul li:has-text('Téléchargement')",
+                          "li[ng-click='showDownloadTree()']",
+                          "Clic sur l'onglet 'Téléchargement'"
+                          )
+        
         # 8. Cliquer sur le bouton de téléchargement des données au format CSV
+
+
+        # fin de fonction
+
